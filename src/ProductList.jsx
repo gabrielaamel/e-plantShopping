4523b16e278e85f9095fcb108bab668a18070b5b
gabrielaamel <gabrielaamel@gmail.com>
2026-08-09 17:15:13 -0400

    Fix import path in ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addItem } from '../CartSlice.jsx';
+import { addItem } from './CartSlice.jsx';
 import CartItem from './CartItem.jsx';
 import './ProductList.css';
 
